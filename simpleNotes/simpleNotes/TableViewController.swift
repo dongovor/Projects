0c3e1f961aa5c90b58659f103251684b1b97ff6e
Dmitry Cherkasov <govor86@gmail.com>
2017-10-18 09:32:59 +0300

    no message

--- simpleNotes/simpleNotes/TableViewController.swift
+++ simpleNotes/simpleNotes/TableViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    @IBAction func addNote(_ sender: Any) {
+        var newDict = ["title":"New Title", "body":"New Body"]
+        self.selectedIndex = 0
+        self.tableView.reloadData()
+        performSegue(withIdentifier: "showDetails", sender: nil)
+    }
+    var notesArr = [[String:String]]()
+    var selectedIndex = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,17 +30,23 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return notesArr.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = "Cell text"
+        cell?.textLabel?.text = notesArr[indexPath.row]["title"]
         return cell!
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        self.selectedIndex = indexPath.row
+        performSegue(withIdentifier: "showDetails", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let notesEditorVC = segue.destination
+        notesEditorVC.navigationItem.title = notesArr[self.selectedIndex]["title"]
     }
 
 
